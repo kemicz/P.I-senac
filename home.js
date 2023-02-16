5381a67802d8636c0ea4174e589b63ede101e4f8
kemicz <kemily.rosa@madeiramadeira.com.br>
2023-02-15 21:37:47 -0300

    refazendo repositorio

---
+++ home.js
@@ -0,0 +1,17 @@
+/* Defina a largura da barra lateral para 250px e a margem esquerda do conteúdo da página para 250px */
+function abrirNav() {
+    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("corpo").style.marginLeft = "250px";
+
+  }
+  
+  /* Defina a largura da barra lateral como 0 e a margem esquerda do conteúdo da página como 0 */
+  function fechaNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("icone").style.marginLeft = "0";
+    document.getElementById("corpo").style.marginLeft = "0";
+  }
+
+
+
+  /* teste */
