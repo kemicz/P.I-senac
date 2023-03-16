8e4f2e0dbc3bb75ae3ab98cb994bd879662785af
Isabel Dutra <isagiodutra1907@gmail.com>
2023-03-16 17:08:47 -0300

    parte 1 do php

---
+++ login-cadastro/cadastro.php
@@ -0,0 +1,15 @@
+<?php
+ if(isset($_POST['cadastrar'])){
+    include_once('conexao.php'); 
+    $nome= $_POST['nome'];
+    $email= $_POST['email'];
+    $senha= $_POST['senha'];
+    $sql= "INSERT INTO cadastro(nome,email,senha) VALUES ('$nome', '$email', '$senha')";
+    $query= mysqli_query($conexao, $sql);
+    if($query){
+        echo "<script>
+         alert('Cadastro realizado com sucesso');
+         window.location.replace('login.php');</script>";
+         }
+    }
+?>
